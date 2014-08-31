7876ac84f6ab23fedeaa9275a9af3aba7627fa98
Yuichi Kuroda <pochi.black@gmail.com>
2014-08-31 23:17:35 +0900

    Add headers for websocket

--- lib/websocket.ex
+++ lib/websocket.ex
@@ -1,7 +1,15 @@
 defmodule Websocket do
   def handshake do
     url = to_char_list(to_string("http://localhost:8080/"))
-    :ibrowse.send_req(url, [], :get)
+    header_list = ['Connection': 'Upgrade',
+                   'Upgrade': 'websocket',
+                   'Sec-WebSocket-Key': 'change-it-later',
+                   'Origin': 'http://localhost/',
+                   'Sec-WebSocket-Protocol': 'change-it-later',
+                   'Sec-WebSocket-Version': '13']
+    headers = Enum.map header_list, fn({k,v}) -> { to_char_list(k), to_char_list(v) } end
+
+    :ibrowse.send_req(url, headers, :get)
   end
 
 end
