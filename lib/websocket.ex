be5c50e68838463f28f9ea07ed286973df104f7b
Yuichi Kuroda <pochi.black@gmail.com>
2014-08-31 22:34:58 +0900

    Create http request

--- lib/websocket.ex
+++ lib/websocket.ex
@@ -1,2 +1,7 @@
 defmodule Websocket do
+  def handshake do
+    url = to_char_list(to_string("http://localhost:8080/"))
+    :ibrowse.send_req(url, [], :get)
+  end
+
 end
--- test/websocket_test.exs
+++ test/websocket_test.exs
@@ -4,4 +4,9 @@ defmodule WebsocketTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "handshake request can send" do
+    { response, _status, _headers, _body } = Websocket.handshake
+    assert response == :ok
+  end
 end
