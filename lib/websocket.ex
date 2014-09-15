f95f82a76732ca49e7e1cc69e8bd9e5b2c5d493c
Yuichi Kuroda <pochi.black@gmail.com>
2014-09-16 00:09:12 +0900

    Refactoring

--- lib/websocket.ex
+++ lib/websocket.ex
@@ -1,12 +1,14 @@
 defmodule Websocket do
+
+  @spec handshake :: tuple
   def handshake do
-    address = String.to_char_list("localhost")
-    port = String.to_integer("8080")
-    origin = "http://localhost/"
+    address = 'localhost'
+    port = 8080
+    origin = 'http://localhost/'
     key = :base64.encode("some websocket key")
 
     { :ok, socket } = :gen_tcp.connect(address, port, [], :infinity)
-    :inet.setopts(socket, packet: :raw)
+    socket |> :inet.setopts(active: true, packet: :raw)
 
     handshake_message = [
       "GET / HTTP/1.1", "\r\n",
@@ -19,15 +21,37 @@ defmodule Websocket do
       "\r\n",
     ]
 
-    response = :gen_tcp.send(socket, handshake_message)
+    response = socket |> :gen_tcp.send(handshake_message)
     { response, socket }
   end
 
-  def loop_acceptor(socket) do
+  def loop_acceptor(client) do
     # activeモードから変更しないとrecvが呼び出せない
     # activeモードのままだと { :error, :einval }がかえってくる
-    :inet.setopts(socket, active: false)
-    :gen_tcp.recv(socket, 0)
+    client |> :inet.setopts(active: false, packet: :http_bin)
+    { :ok, { :http_response, _, status_code, switch_message } } = client |> :gen_tcp.recv(0)
+    http_headers = client |> headers
+    { status_code, switch_message, http_headers }
+  end
+
+  def ping(client) do
+    client |> :inet.setopts(active: true, packet: :raw)
+    response = client |> :gen_tcp.send("pingpingping")
+    { response, client }
+  end
+
+  defp headers(client) do
+    headers([], client)
+  end
+
+  defp headers(http_header, client) do
+    case client |> :gen_tcp.recv(0) do
+      {:ok, {:http_header, _, name, _, val } } ->
+         http_header ++ [{name, val}] |> headers(client)
+      {:ok, :http_eoh } ->
+         http_header
+    end
+
   end
 
 end
--- test/websocket_test.exs
+++ test/websocket_test.exs
@@ -12,8 +12,16 @@ defmodule WebsocketTest do
 
   test "handshake response should receive" do
     { _, socket } = Websocket.handshake
-    { response, data } = Websocket.loop_acceptor(socket)
-    assert data == response_template_direct
+    { status_code, data, _ } = Websocket.loop_acceptor(socket)
+    assert status_code == 101
+    assert data == "Switching Protocols"
+  end
+
+  test "socket can send ping request" do
+    { _, socket } = Websocket.handshake
+    { _, _, _ } = Websocket.loop_acceptor(socket)
+    { response, _ } = Websocket.ping(socket)
+    assert response == :ok
   end
 
 end
